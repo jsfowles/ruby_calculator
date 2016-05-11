ccdb4bdb2b3200f638d0d2bb38632276dbcb8228
Jacob Fowles <js.fowles@gmail.com>
2016-05-11 12:10:57 -0600

    more psuedo code

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -5,3 +5,13 @@
 
 puts "Welcome to the Ruby calculator."
 puts "Enter your first number:"
+# get the first number
+puts "Enter our operator"
+# get the operator
+puts "Enter the second number:"
+# get the second number
+
+# figure out how to do the math on the two numbers
+# output the result to the user
+
+# Do all the bous problems
