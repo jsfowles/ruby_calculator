dd761cf5818070fe8b0513db979cef51d58d6479
Jacob Fowles <js.fowles@gmail.com>
2016-05-11 15:36:56 -0600

    a basic working file

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -2,14 +2,69 @@
 # stage the file - git add
 # make a comiit
 # push to a github repo
+# operators = ['+', '-', '*', '/']
 
+def add(first_val, sec_val)
+  result = first_val + sec_val
+  puts result
+end
+
+def subtract(first_val, sec_val)
+  result1 = first_val - sec_val
+  puts result1
+end
+
+def multiple(first_val, sec_val)
+  result2 = first_val * sec_val
+  puts result2
+end
+
+def divide(first_val, sec_val)
+  result3 = first_val / sec_val
+  puts result3
+end
+
+def choice
 puts "Welcome to the Ruby calculator."
 puts "Enter your first number:"
+first_val = gets.to_f
+
 # get the first number
-puts "Enter our operator"
+puts "Enter your operator"
+operator = gets.chomp
+
 # get the operator
 puts "Enter the second number:"
+sec_val = gets.to_f
+
+puts "Calculating..."
+
+  case operator
+    when "+"
+      add(first_val, sec_val)
+    when "-"
+      subtract(first_val, sec_val)
+    when "*"
+      multiple(first_val, sec_val)
+    when "/"
+      divide(first_val, sec_val)
+    else
+      error_mes
+  end
+end
+
+def error_mes
+  puts "Try again..."
+  choice
+end
+
+choice
+
 # get the second number
+
+
+
+
 
 # figure out how to do the math on the two numbers
 # output the result to the user
